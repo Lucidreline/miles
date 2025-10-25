0604e9ee4f41ec67c57b3256bd7868ced356d4d0
Lucidreline <casta.ma502@gmail.com>
2025-10-25 15:29:38 -0700

    Gutted the whole app.

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // this server section makes it possible to auto restart server when a file change is made in a dev container
+  server: {
+    host: true,
+    watch: {
+      usePolling: true
+    }
+  }
 })
