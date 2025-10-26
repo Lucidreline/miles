c69b4af77728267db282960b00585dd38308e315
Lucidreline <casta.ma502@gmail.com>
2025-10-26 12:17:55 -0700

    Styled up a simple nav bar.

--- src/components/navBar/NavBar.component.jsx
+++ src/components/navBar/NavBar.component.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './NavBar.styles.scss'
+import { Link } from 'react-router-dom'
 
 const NavBar = () => {
+    const [isHamburgerOpen, setIsHamburgerOpen] = useState(false)
+
+    // this will close the menu once a link is clicked
+    const closeHamburger = () => {
+        setIsHamburgerOpen(false)
+    }
+
     return (
-        <div>NavBar</div>
+        <div className="nav-bar">
+            <div className="nav-home">
+                <Link to="/" onClick={closeHamburger}>Miles</Link>
+            </div>
+            <div className={`nav-links ${isHamburgerOpen ? 'open' : ''}`}>
+                <ul>
+                    <li>
+                        <Link to="/" onClick={closeHamburger}>Logs</Link>
+                    </li>
+                    <li>
+                        <Link to="/" onClick={closeHamburger}>Challenges</Link>
+                    </li>
+                    <li>
+                        <Link to="/" onClick={closeHamburger}>Land Marks</Link>
+                    </li>
+                </ul>
+            </div>
+        </div >
     )
 }
 
